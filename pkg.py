8010237abbd5db432e91a5737d05d8fd71c15f48
ahnjy7168 <128241524+ahnjy7168@users.noreply.github.com>
2023-07-08 00:52:56 +0900

    Pkg.py

---
+++ pkg.py
@@ -0,0 +1,21 @@
+import os
+
+os.system("clear")
+print("""
+
+
+
+
+
+
+
+
+""")
+os.system("pkg update")
+os.system("pkg upgrade")
+os.system("pkg install git")
+os.system("pkg install wget")
+os.system("pkg install openssh")
+os.system("pkg install python")
+os.system("pkg install nano")
+os.system("clear")
